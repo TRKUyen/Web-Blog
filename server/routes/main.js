d366325448bc5faa24b3808d2cb15a8d769dfabf
TRKUyen <kimuyen425@gmail.com>
2024-08-17 15:08:40 +0700

    Add new images, header & footer page, update index.ejs main home page

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,11 +3,20 @@ const router = express.Router();
 
 //Routers
 router.get("", (req, res) => {
-  res.render("index");
+  const locals = {
+    title: "NodeJS Blog Web",
+    description: "Simple Blog Web make by NodeJS, Express & MongoDB.",
+  };
+
+  res.render("index", { locals });
 });
 
 router.get("/about", (req, res) => {
   res.render("about");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
